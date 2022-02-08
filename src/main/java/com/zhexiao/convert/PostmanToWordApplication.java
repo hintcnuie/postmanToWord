8641da3051dc4725cedfa0fdadfc79eb276e4881
chenzw <developer@deltac.com.cn>
2022-02-08 11:52:11 +0800

    增加war 打包功能

--- src/main/java/com/zhexiao/convert/PostmanToWordApplication.java
+++ src/main/java/com/zhexiao/convert/PostmanToWordApplication.java
@@ -2,12 +2,22 @@ package com.zhexiao.convert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+/**
+ * PostmanToWordApplication
+ * 
+ * @author CharlemanChen
+ *
+ */
 @SpringBootApplication
 public class PostmanToWordApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostmanToWordApplication.class, args);
     }
-
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(PostmanToWordApplication.class);
+    }
 }
